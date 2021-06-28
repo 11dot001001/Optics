fb72d3e9ac1c41e69c8715b938d32a34975c5f17
alexsey chuvilkin <leha.chuvilkin.90@mail.ru>
2021-06-29 00:48:31 +0300

    Added light reflection through relative position regarding surface.

--- Assets/Scripts/LightRay.cs
+++ Assets/Scripts/LightRay.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -11,7 +12,7 @@ namespace Optics
 		private Vector3 _initialDirection;
 
 		public LightRenderer LightRenderer;
-
+		
 		public void Initialize(Vector3 position, Vector3 direction)
 		{
 			_initialPosition = position;
@@ -43,18 +44,37 @@ namespace Optics
 				Normal = hit.normal
 			};
 
-			Vector3 wallPosition = wallPoint.Position;
-			Vector3 wallSurfaceNormal = wallPoint.Normal;
-			Vector3 lightDirection = wallPosition - startPosition;
-			
-			Vector3 reverseLightDirection = lightDirection * -1;
-			Vector3 rotationNormal = Vector3.Cross(reverseLightDirection, wallSurfaceNormal).normalized;
-			
-			float entryAngleDegrees = Vector3.SignedAngle(reverseLightDirection, wallSurfaceNormal, rotationNormal); // equal to reflection
-			Quaternion rotation = Quaternion.AngleAxis(entryAngleDegrees, rotationNormal);
-			Vector3 reflectedLightNormalizedDirection = (rotation * wallSurfaceNormal).normalized;
+			Vector3 reflectedLightNormalizedDirection = GetReflectedLightDirectionViaRelativePosition(
+				startPosition, 
+				wallPoint.Position,
+				wallPoint.Normal
+			);
 			
 			GeneratePathPosition(wallPoint.Position, reflectedLightNormalizedDirection,walls);
+		}
+		
+		/// <summary>
+		/// Rotates surface normal vector on incidence angle in plane of incidence.
+		/// </summary>
+		private Vector3 GetReflectedLightDirectionViaRotation(Vector3 startPosition, Vector3 surfacePosition, Vector3 surfaceNormal)
+		{
+			Vector3 lightDirection = surfacePosition - startPosition;
+			Vector3 reverseLightDirection = lightDirection * -1;
+			Vector3 rotationNormal = Vector3.Cross(reverseLightDirection, surfaceNormal).normalized;
+			
+			float entryAngleDegrees = Vector3.SignedAngle(reverseLightDirection, surfaceNormal, rotationNormal); // equal to reflection
+			Quaternion rotation = Quaternion.AngleAxis(entryAngleDegrees, rotationNormal);
+			return (rotation * surfaceNormal).normalized;
+		}
+		/// <summary>
+		/// Returns reflected direction via relative start position regarding surface.
+		/// </summary>
+		private Vector3 GetReflectedLightDirectionViaRelativePosition(Vector3 startPosition, Vector3 surfacePosition, Vector3 surfaceNormal)
+		{
+			float startPositionToSurfaceMagnitude = Math.Abs(Vector3.Dot(surfaceNormal, startPosition) - Vector3.Dot(surfaceNormal, surfacePosition));
+			
+			Vector3 relativeStartPositionViaSurface = startPosition + surfaceNormal.normalized * startPositionToSurfaceMagnitude * -2;
+			return (surfacePosition - relativeStartPositionViaSurface).normalized;
 		}
 	}
 }
